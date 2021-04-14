7dce62d3b78fec43ef43dea0c90de7a055bac259
qngnhat <qngnhat@gmail.com>
2021-04-15 05:54:53 +0700

    first commit

---
+++ src/main/java/com/nez/NezSpringSecurApplication.java
@@ -0,0 +1,13 @@
+package com.nez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NezSpringSecurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NezSpringSecurApplication.class, args);
+	}
+
+}
